e483e8902c561ed2bc1cd8603894569741d25f62
Ken Kambi <kenkambi43@gmail.com>
2025-05-20 20:49:34 +0300

    static template

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,11 +1,33 @@
+import "../footer/style.css";
 
-import "../footer/style.css"
-
-export default function Footer (){
-
-    return (
-        <footer>
-        <small> &copy; {new Date().getFullYear()}. Thank you for visiting our website. </small>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        {" "}
+        &copy; {new Date().getFullYear()}. Thank you for visiting our website.{" "}
+      </small>
+      <div>
+        <a href="youtube.com">
+          <img src="youtubelogo" alt="Youtube Logo" />
+        </a>
+        <a href="twitter.com">
+          <img src="youtubelogo" alt="Youtube Logo" />
+          Twitter
+        </a>
+        <a href="tiktok.com">
+          <img src="youtubelogo" alt="Youtube Logo" />
+          TikTok
+        </a>
+        <a href="instagram.com">
+          <img src="youtubelogo" alt="Youtube Logo" />
+          Instagram
+        </a>
+        <a href="whatsapp.com">
+          <img src="youtubelogo" alt="Youtube Logo" />
+          Whatsapp
+        </a>
+      </div>
+    </footer>
+  );
 }
