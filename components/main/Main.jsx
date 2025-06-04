2620df43e1298c2d2a070da5b724025a6ab2bcb7
KenKambi <122309458+KenKambi@users.noreply.github.com>
2025-06-04 23:14:52 +0300

    Update Main.jsx

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -4,8 +4,6 @@ import "../main/style.css";
 export default function Main(props) {
    //console.log(props.item.id)
   return (
-    
-
     <section>
       <h3> Name </h3>
       <img src={props.item.image} alt="Perfume " />
